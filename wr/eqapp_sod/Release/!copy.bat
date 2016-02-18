0baa8586cbfbe404bfed59c90b42481c57ab2281
unknown <unknown>
2016-02-17 18:48:39 -0600

    .

--- wr/eqapp_sod/Release/!copy.bat
+++
@@ -1,3 +0,0 @@
-echo d | xcopy /f /y /e D:\Code\everquest\eqapp_sod\Release\eqapp C:\EQDalaya\eqapp
-echo f | xcopy /f /y D:\Code\everquest\eqapp_sod\Release\eqapp_sod.dll C:\EQDalaya\eqapp.dll
-pause
